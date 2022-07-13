88653b1a14316361ed2c2d5cd3ecc99130d16a8c
Alexander Sadowski <sania.nestanda@yandex.ru>
2022-07-13 17:15:01 +0300

    Добавляет подстановку фото в форму редактирования

--- js/form.js
+++ js/form.js
@@ -4,10 +4,12 @@ import { showStatusPopup } from './status-popup.js';
 
 const HASHTAGS_COUNT = 5;
 const HASHTAG_REGULAR_EXPRESSION = /^#[A-Za-zА-ЯаяЁё0-9]{1,19}$/;
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const formElement = document.querySelector('.img-upload__form');
 const fileInputElement = formElement.querySelector('#upload-file');
 const uploadOverlayElement = formElement.querySelector('.img-upload__overlay');
+const imageElement = formElement.querySelector('.img-upload__preview img');
 const formCloseElement = formElement.querySelector('#upload-cancel');
 const hashtagsInputElement = formElement.querySelector('.text__hashtags');
 const formSubmitElement = formElement.querySelector('.img-upload__submit');
@@ -40,6 +42,14 @@ pristine.addValidator(hashtagsInputElement, checkUniquenessHashtags, 'Хэш-т�
 pristine.addValidator(hashtagsInputElement, checkHashtagsCount, `Можно указать не более ${HASHTAGS_COUNT} хэш-тегов.`, 3, true);
 
 const onFileInputChange = () => {
+  const file = fileInputElement.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((extension) => fileName.endsWith(extension));
+
+  if (matches) {
+    imageElement.src = URL.createObjectURL(file);
+  }
+
   openModal(uploadOverlayElement);
 };
 
